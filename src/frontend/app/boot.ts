5981e98a043e866f321badaf0f4f5b119c43f829
beforeadream <beforeadream@gmail.com>
2016-08-01 13:18:39 +0300

    start over

--- src/frontend/app/boot.ts
+++
@@ -1,7 +0,0 @@
-import {enableProdMode} from 'angular2/core';  
-import {bootstrap} from 'angular2/platform/browser';  
-import {AppComponent} from './components/app';
-
-enableProdMode();
-
-bootstrap(AppComponent, []);  
