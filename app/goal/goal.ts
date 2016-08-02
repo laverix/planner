60d3a44f8af22b05962b3d63a4745a85b200034d
beforeadream <beforeadream@gmail.com>
2016-08-02 14:53:09 +0300

    Finished layout with mock data

---
+++ app/goal/goal.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'goal',
+    templateUrl: './app/goal/goal.html',
+    styleUrls: ['./app/goal/goal.css'],
+    encapsulation: ViewEncapsulation.Native
+})
+
+export class GoalComponent {}
--- app/spheres/spheres.ts
+++ app/spheres/spheres.ts
@@ -1,10 +1,13 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
+import { GoalComponent } from './../goal/goal';
+
 @Component({
     selector: 'spheres',
     templateUrl: 'app/spheres/spheres.html',
     styleUrls: ['./app/spheres/spheres.css'],
-    encapsulation: ViewEncapsulation.Native
+    encapsulation: ViewEncapsulation.Native,
+    directives: [GoalComponent]
 })
 
 export class SpheresComponent {}
